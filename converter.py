a020c6f70d5d1c5ed5743bb4925e4aec7ff6c34b
John C. Vernaleo <john@netpurgatory.com>
2018-09-04 10:43:02 -0400

    Cleanup and pep8

--- converter.py
+++ converter.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-#john@netpurgatory.com
-#09/03/2018
+# john@netpurgatory.com
+# 09/03/2018
 
 from lxml import etree
 import csv
+
 
 def readCsv(inFile):
     cardData = []
@@ -13,20 +14,21 @@ def readCsv(inFile):
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
-                print(row)
                 line_count += 1
             else:
                 cardData.append(row)
                 line_count += 1
-        print('Processed ',line_count,' lines.')
+        print('Read '+str(line_count-1)+' cards.')
     return(cardData)
+
 
 def genXml(outFile, inData):
     root = etree.Element("cockatrice_carddatabase", version="3")
     cards = etree.SubElement(root, "cards")
 
+    line_count = 0
     for cData in inData:
-        print cData["Name"]
+        line_count += 1
         card = etree.SubElement(cards, "card")
         etree.SubElement(card, "name").text = cData["Name"]
         etree.SubElement(card, "type").text = cData["Card Type"]
@@ -48,10 +50,10 @@ def genXml(outFile, inData):
     # </card>
 
     res = etree.tostring(root, pretty_print=True)
-    print(res)
     xml_file = open(outFile, "w")
     xml_file.write(res)
     xml_file.close()
+    print('Wrote '+str(line_count)+' lines.')
 
-cardData=readCsv("cards.csv")
+cardData = readCsv("cards.csv")
 genXml("output.xml", cardData)
