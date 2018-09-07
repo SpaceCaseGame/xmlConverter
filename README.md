e8ab7e669b931ea69898c2f0f820ad149dd1272f
John C. Vernaleo <john@netpurgatory.com>
2018-09-07 08:38:22 -0400

    Add skills to output

--- README.md
+++ README.md
@@ -4,17 +4,19 @@ Simple tool to convert to Cockatrice format xml.
 
 ## Usage
 
-This expects a csv file as input which is specified on the command line.  The file must not contain any special characters.  You may remove thos with:
+This expects a 2 csv files as input which is specified on the command line.  The file must not contain any special characters.  You may remove those with:
 
 ```
 perl -pi -e 's/Æ/AE/g' Cards\ CSV.csv
 perl -pi -e 's/æ/ae/g' Cards\ CSV.csv
 perl -pi -e 's/•/*/g' Cards\ CSV.csv
+perl -pi -e 's/Æ/AE/g' Skills_CSV.csv
+perl -pi -e 's/æ/ae/g' Skills_CSV.csv
 ```
 
 Then run the code:
 ```
-./converter.py Cards\ CSV.csv
+./converter.py Cards\ CSV.csv Skills_CSV.csv
 ```
 
 ## License
--- converter.py
+++ converter.py
@@ -9,6 +9,11 @@ import sys
 
 
 def readCsv(inFile):
+    """Reads exported card data
+    This takes in a csv file where the first line containers
+    the headers that will be used to determine the field names
+    and returns a list of dictionaries.
+    """
     cardData = []
     with open(inFile) as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
@@ -23,14 +28,18 @@ def readCsv(inFile):
     return(cardData)
 
 
-def genXml(outFile, cardData, skillData):
+def genXml(outFile, verbose, cardData, skillData):
+    """Generate xml file based for Cockatrice
+    This expects both cardData and skillData as lists of dictionaries.
+    """
     root = etree.Element("cockatrice_carddatabase", version="3")
     cards = etree.SubElement(root, "cards")
 
     line_count = 0
     for cData in cardData:
         line_count += 1
-        # print(cData["Name"])
+        if verbose:
+            print(cData["Name"])
         card = etree.SubElement(cards, "card")
         etree.SubElement(card, "name").text = cData["Name"]
         # <set picURL=""></set> Ignore
@@ -44,14 +53,14 @@ def genXml(outFile, cardData, skillData):
         # <loyalty></loyalty> Ignore
         tableRow = ""
         if cData["Card Type"] == "Gear":
-            if cData["Subtypes(s)"] == "Armor":
-                tableRow = 0
-            if cData["Subtypes(s)"] == "Weapons":
-                tableRow = 2
+            if cData["Subtype(s)"] == "Armor":
+                tableRow = "0"
+            if cData["Subtype(s)"] == "Weapons":
+                tableRow = "2"
         if cData["Card Type"] == "AEon":
-            tableRow = 1
+            tableRow = "1"
         if cData["Card Type"] == "Talent":
-            tableRow = 3
+            tableRow = "3"
         etree.SubElement(card, "tablerow").text = tableRow
         etree.SubElement(card, "text").text = cData["Intrinsic"]
         # etree.SubElement(card,
@@ -59,6 +68,14 @@ def genXml(outFile, cardData, skillData):
         # <cipt></cipt> Ignore
     for cData in skillData:
         line_count += 1
+        if verbose:
+            print(cData["Name"])
+        card = etree.SubElement(cards, "card")
+        etree.SubElement(card, "name").text = cData["Name"]
+        etree.SubElement(card, "cmc").text = cData["Cost"]
+        etree.SubElement(card, "type").text = cData["AET Type"]
+        etree.SubElement(card, "text").text = cData["Text"]
+        etree.SubElement(card, "token").text = "1"
     res = etree.tostring(root, pretty_print=True)
     xml_file = open(outFile, "w")
     xml_file.write(res)
@@ -73,4 +90,4 @@ if __name__ == "__main__":
     cardData = readCsv(cardFile)
     skillFile = sys.argv[2]
     skillData = readCsv(skillFile)
-    genXml("output.xml", cardData)
+    genXml("output.xml", False, cardData, skillData)
